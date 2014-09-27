6b87b2a409631c430751e7a07602ad24dec2f2b7
borislav nikolov <jack@sofialondonmoskva.com>
2014-09-27 18:36:41 +0200

    randomize the test

--- Inverted-StoredList/bench.pl
+++ Inverted-StoredList/bench.pl
@@ -8,29 +8,41 @@ my @terms = ("jack","doe","foo","bar");
 my $t0 = time();
 my $n = 0;
 my $per_report = 100_000;
+
 srand;
 for my $id(1..100_000_000) {
     my @t = ();
-=begin
+
     for my $term(@terms) {
         push @t,$term if (int(rand(2)) == 1);
     }
-=cut
+
     $i->index(\@terms,$id);
     if ($n != 0 && ($n % $per_report) == 0) {
 
         my $diff = time() - $t0;
         $t0 = time();
         print sprintf "$n: took <%.2f> for %d or %.2f per second\n",$diff,$per_report,$per_report / $diff;
+
     }
     $n++;
 }
 
-
-
+$t0 = time();
 my $result = $i->search({
     must => [
         { term => "jack" },
         { term => "doe" },
     ]
 });
+print Dumper([time() - $t0,$result]);
+
+
+$t0 = time();
+my $result = $i->search({
+    must => [
+        { term => "jack" },
+        { term => "doe" },
+    ]
+});
+print Dumper([time() - $t0,$result]);
